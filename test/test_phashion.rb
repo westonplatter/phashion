88a72f7fa63b36c1d0e4ff62929033547e277055
Mike Perham <mperham@gmail.com>
2010-05-21 21:35:34 -0500

    Add test to verify original images, not generated thumbnails!

--- test/test_phashion.rb
+++ test/test_phashion.rb
@@ -10,6 +10,14 @@ class TestPhashion < Test::Unit::TestCase
     assert_duplicate images[0], images[2]
   end
 
+  def test_duplicate_detection_2
+    files = %w(b32aade8c590e2d776c24f35868f0c7a588f51e1.jpeg df9cc82f5b32d7463f36620c61854fde9d939f7f.jpeg e7397898a7e395c2524978a5e64de0efabf08290.jpeg)
+    images = files.map {|f| Phashion::Image.new("#{File.dirname(__FILE__) + '/../test/'}#{f}")}
+    assert_duplicate images[0], images[1]
+    assert_duplicate images[1], images[2]
+    assert_duplicate images[0], images[2]
+  end
+
   def test_not_duplicate
     files = %w(86x86-0a1e.jpeg 86x86-83d6.jpeg 86x86-a855.jpeg avatar.jpg)
     images = files.map {|f| Phashion::Image.new("#{File.dirname(__FILE__) + '/../test/'}#{f}")}
