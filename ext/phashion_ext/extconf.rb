1706df6e00296e4b2318b475cb0223fc26f69a13
Mike Perham <mperham@gmail.com>
2010-06-04 21:27:08 -0500

    Build custom version of pHash locally to make installation painless.

--- ext/phashion_ext/extconf.rb
+++ ext/phashion_ext/extconf.rb
@@ -1,13 +1,42 @@
 require 'mkmf'
 
-$CFLAGS << " -x c++ #{ENV["CFLAGS"]}"
-$LIBS << " -lpHash #{ENV["LIBS"]}"
+HERE = File.expand_path(File.dirname(__FILE__))
+BUNDLE = Dir.glob("#{HERE}/pHash-*.tar.gz").first
+BUNDLE_PATH = BUNDLE.gsub(".tar.gz", "")
+$CFLAGS = " -x c++ #{ENV["CFLAGS"]}"
+$includes = " -I#{HERE}/include"
+$libraries = " -L#{HERE}/lib"
+$LIBPATH = ["#{HERE}/lib"]
+$CFLAGS = "#{$includes} #{$libraries} #{$CFLAGS}"
+$LDFLAGS = "#{$libraries} #{$LDFLAGS}"
 
-# TODO: need to figure this stuff out
-# dir_config 'pHash'
-# if !have_library('pHash', 'ph_dct_imagehash')
-#   puts "Unable to find pHash library, please use 'gem install phashion -- --with-pHash-dir=/phash/install/root'"
-#   exit 1
-# end
+Dir.chdir(HERE) do
+  if File.exist?("lib")
+    puts "pHash already built; run 'rake clean' first if you need to rebuild."
+  else
 
-create_makefile('phashion_ext')
+    puts(cmd = "tar xzf #{BUNDLE} 2>&1")
+    raise "'#{cmd}' failed" unless system(cmd)
+
+    Dir.chdir(BUNDLE_PATH) do
+      puts(cmd = "env CFLAGS='#{$CFLAGS}' LDFLAGS='#{$LDFLAGS}' ./configure --prefix=#{HERE} --disable-audio-hash --disable-video-hash --with-pic 2>&1")
+      raise "'#{cmd}' failed" unless system(cmd)
+
+      puts(cmd = "make || true 2>&1")
+      raise "'#{cmd}' failed" unless system(cmd)
+
+      puts(cmd = "make install || true 2>&1")
+      raise "'#{cmd}' failed" unless system(cmd)
+    end
+
+    system("rm -rf #{BUNDLE_PATH}") unless ENV['DEBUG'] or ENV['DEV']
+  end
+  
+  Dir.chdir("#{HERE}/lib") do
+    system("cp -f libpHash.a libpHash_gem.a")
+    system("cp -f libpHash.la libpHash_gem.la")
+  end
+  $LIBS = " -lpHash_gem"
+end
+
+create_makefile 'phashion_ext'
