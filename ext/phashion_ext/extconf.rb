ae71268e6858a2e416b6b285c22ebf1589011220
Ariejan de Vroom <ariejan@ariejan.net>
2011-07-20 06:42:16 -0700

    Use pthread explicitly; needed for Ubuntu 11.04.

--- ext/phashion_ext/extconf.rb
+++ ext/phashion_ext/extconf.rb
@@ -39,7 +39,7 @@ Dir.chdir(HERE) do
     system("cp -f libpHash.a libpHash_gem.a")
     system("cp -f libpHash.la libpHash_gem.la")
   end
-  $LIBS = " -lpHash_gem -lstdc++ -ljpeg"
+  $LIBS = " -lpthread -lpHash_gem -lstdc++ -ljpeg"
 end
 
 create_makefile 'phashion_ext'
