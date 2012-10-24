08e312359700ec7d2bb0df3e91a939b0c3c67044
Ariejan de Vroom <ariejan@ariejan.net>
2012-10-25 00:13:32 +0300

    Disable compiling with pthread

--- ext/phashion_ext/extconf.rb
+++ ext/phashion_ext/extconf.rb
@@ -19,7 +19,7 @@ Dir.chdir(HERE) do
     raise "'#{cmd}' failed" unless system(cmd)
 
     Dir.chdir(BUNDLE_PATH) do
-      puts(cmd = "env CFLAGS='#{$CFLAGS}' LDFLAGS='#{$LDFLAGS}' ./configure --prefix=#{HERE} --disable-audio-hash --disable-video-hash --disable-shared --with-pic 2>&1")
+      puts(cmd = "env CFLAGS='#{$CFLAGS}' LDFLAGS='#{$LDFLAGS}' ./configure --prefix=#{HERE} --disable-audio-hash --disable-video-hash --disable-pthread --disable-shared --with-pic 2>&1")
       raise "'#{cmd}' failed" unless system(cmd)
 
       puts(cmd = "make || true 2>&1")
