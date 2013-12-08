2af3a9e544ebfc7d07ec73c89216b75da29804a6
Aaron Patterson <aaron.patterson@gmail.com>
2013-12-08 15:58:12 -0800

    unlock the GVL while calculating a hash

--- ext/phashion_ext/phashion_ext.c
+++ ext/phashion_ext/phashion_ext.c
@@ -1,13 +1,47 @@
 #include "ruby.h"
 #include "pHash.h"
+#ifdef HAVE_RUBY_THREAD_H
+#include <ruby/thread.h>
+#else
+#include <ruby/intern.h>
+void *
+rb_thread_call_without_gvl(void *(*func)(void *data), void *data1,
+			    rb_unblock_function_t *ubf, void *data2) {
+  return (void *)rb_thread_blocking_region(
+      (rb_blocking_function_t *)func, data1,
+      (rb_unblock_function_t *)ubf, data2);
+}
+#endif
+
+struct nogvl_hash_args {
+  const char * filename;
+  ulong64 hash;
+  int retval;
+};
+
+static void * nogvl_hash(struct nogvl_hash_args * args) {
+  ulong64 hash;
+
+  args->retval = ph_dct_imagehash(args->filename, hash);
+  args->hash = hash;
+
+  return NULL;
+}
 
 static VALUE image_hash_for(VALUE self, VALUE _filename) {
-    char * filename = StringValuePtr(_filename);
     ulong64 hash;
-    if (-1 == ph_dct_imagehash(filename, hash)) {
+    struct nogvl_hash_args args;
+
+    args.filename = StringValuePtr(_filename);
+    args.retval = -1;
+
+    rb_thread_call_without_gvl((void *(*)(void *))nogvl_hash,
+        (void *)&args, RUBY_UBF_PROCESS, 0);
+
+    if (-1 == args.retval) {
       rb_raise(rb_eRuntimeError, "Unknown pHash error");
     }
-    return ULL2NUM(hash);
+    return ULL2NUM(args.hash);
 }
 
 
